87e876db797155853d0e50e3981b6cd95895f85e
Johan Solbakken <johsol@stud.ntnu.no>
2023-11-01 15:21:27 +0100

    V1

---
+++ 2/simple_indexer.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import os
+import string
+from nltk.stem import PorterStemmer
+
+class SimpleIndexer:
+    def __init__(self) -> None:
+        self.__documents = []
+        self.__index = {}
+        self.__id = 0
+        self.__stop_words = ['a', 'about', 'above', 'across', 'after', 'again', 'against', 'all', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'among', 'an', 'and', 'another', 'any', 'anybody', 'anyone', 'anything', 'anywhere', 'are', 'area', 'areas', 'around', 'as', 'ask', 'asked', 'asking', 'asks', 'at', 'away', 'b', 'back', 'backed', 'backing', 'backs', 'be', 'became', 'because', 'become', 'becomes', 'been', 'before', 'began', 'behind', 'being', 'beings', 'best', 'better', 'between', 'big', 'both', 'but', 'by', 'c', 'came', 'can', 'cannot', 'case', 'cases', 'certain', 'certainly', 'clear', 'clearly', 'come', 'could', 'd', 'did', 'differ', 'different', 'differently', 'do', 'does', 'done', 'down', 'down', 'downed', 'downing', 'downs', 'during', 'e', 'each', 'early', 'either', 'end', 'ended', 'ending', 'ends', 'enough', 'even', 'evenly', 'ever', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'f', 'face', 'faces', 'fact', 'facts', 'far', 'felt', 'few', 'find', 'finds', 'first', 'for', 'four', 'from', 'full', 'fully', 'further', 'furthered', 'furthering', 'furthers', 'g', 'gave', 'general', 'generally', 'get', 'gets', 'give', 'given', 'gives', 'go', 'going', 'good', 'goods', 'got', 'great', 'greater', 'greatest', 'group', 'grouped', 'grouping', 'groups', 'h', 'had', 'has', 'have', 'having', 'he', 'her', 'here', 'herself', 'high', 'high', 'high', 'higher', 'highest', 'him', 'himself', 'his', 'how', 'however', 'i', 'if', 'important', 'in', 'interest', 'interested', 'interesting', 'interests', 'into', 'is', 'it', 'its', 'itself', 'j', 'just', 'k', 'keep', 'keeps', 'kind', 'knew', 'know', 'known', 'knows', 'l', 'large', 'largely', 'last', 'later', 'latest', 'least', 'less', 'let', 'lets', 'like', 'likely', 'long', 'longer', 'longest', 'm', 'made', 'make', 'making', 'man', 'many', 'may', 'me', 'member', 'members', 'men', 'might', 'more', 'most', 'mostly', 'mr', 'mrs', 'much', 'must', 'my', 'myself', 'n', 'necessary', 'need', 'needed', 'needing', 'needs', 'never', 'new', 'new', 'newer', 'newest', 'next', 'no', 'nobody', 'non', 'noone', 'not', 'nothing', 'now', 'nowhere', 'number', 'numbers', 'o', 'of', 'off', 'often', 'old', 'older', 'oldest', 'on', 'once', 'one', 'only', 'open', 'opened', 'opening', 'opens', 'or', 'order', 'ordered', 'ordering', 'orders', 'other', 'others', 'our', 'out', 'over', 'p', 'part', 'parted', 'parting', 'parts', 'per', 'perhaps', 'place', 'places', 'point', 'pointed', 'pointing', 'points', 'possible', 'present', 'presented', 'presenting', 'presents', 'problem', 'problems', 'put', 'puts', 'q', 'quite', 'r', 'rather', 'really', 'right', 'right', 'room', 'rooms', 's', 'said', 'same', 'saw', 'say', 'says', 'second', 'seconds', 'see', 'seem', 'seemed', 'seeming', 'seems', 'sees', 'several', 'shall', 'she', 'should', 'show', 'showed', 'showing', 'shows', 'side', 'sides', 'since', 'small', 'smaller', 'smallest', 'so', 'some', 'somebody', 'someone', 'something', 'somewhere', 'state', 'states', 'still', 'still', 'such', 'sure', 't', 'take', 'taken', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'therefore', 'these', 'they', 'thing', 'things', 'think', 'thinks', 'this', 'those', 'though', 'thought', 'thoughts', 'three', 'through', 'thus', 'to', 'today', 'together', 'too', 'took', 'toward', 'turn', 'turned', 'turning', 'turns', 'two', 'u', 'under', 'until', 'up', 'upon', 'us', 'use', 'used', 'uses', 'v', 'very', 'w', 'want', 'wanted', 'wanting', 'wants', 'was', 'way', 'ways', 'we', 'well', 'wells', 'went', 'were', 'what', 'when', 'where', 'whether', 'which', 'while', 'who', 'whole', 'whose', 'why', 'will', 'with', 'within', 'without', 'work', 'worked', 'working', 'works', 'would', 'x', 'y', 'year', 'years', 'yet', 'you', 'young', 'younger', 'youngest', 'your', 'yours', 'z']
+        self.__stemmer = PorterStemmer()
+        self.__stop_words = [self.__stemmer.stem(word) for word in self.__stop_words]
+
+    def index_document(self, document:str):
+        id = self.next_id()
+        self.__documents.append((id,document))
+        tokens = self.preprocess(document)
+        vocabulary = sorted(set(tokens))
+        for word in vocabulary:
+            if word not in self.__index:
+                self.__index[word] = dict()
+            if id not in self.__index[word]:
+                self.__index[word][id] = dict()
+            self.__index[word][id]["count"] = tokens.count(word)
+            self.__index[word][id]["positions"] = []
+        for word in vocabulary:
+            for i in range(len(document)):
+                if document[i:i+len(word)].lower() == word:
+                    self.__index[word][id]["positions"].append(i)
+
+    def preprocess(self, document:str):
+        document = document.lower()
+        for p in string.punctuation:
+            document = document.replace(p, "")
+        document = document.split()
+        document = [self.__stemmer.stem(word) for word in document]
+        document = [word for word in document if word not in self.__stop_words]
+        return document
+
+    def id(self):
+        return self.__id
+    
+    def next_id(self):
+        id = self.__id
+        self.__id += 1
+        return id
+    
+    def documents(self):
+        return self.__documents
+    
+    def index(self):
+        return self.__index
+    
+    def get_document(self, id):
+        for doc_id, doc in self.__documents:
+            if doc_id == id:
+                return doc
+        return None
+
+def main():
+    if not os.path.exists("DataAssignment4"):
+        print("DataAssignment4 folder not found")
+        return
+    
+    indexer = SimpleIndexer()
+
+    for file in os.listdir("DataAssignment4"):
+        if file.endswith(".txt"):
+            with open(os.path.join("DataAssignment4", file), "r") as f:
+                indexer.index_document(f.read())
+
+    for word in indexer.index():
+        print(word, indexer.index()[word], sep="\t")
+
+
+if __name__ == "__main__":
+    main()
